3df7127aacff820f68333a23ce4bb5ecd4fe3e79
tangshi <tangshi@t.shu.edu.cn>
2017-08-08 00:08:52 +0800

    remove unnecessary labels

--- labelvalidator.cpp
+++ labelvalidator.cpp
@@ -12,10 +12,7 @@ QValidator::State LabelValidator::validate(QString &input, int &) const
     {
         const static QStringList kLabels {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-            "A", "B", "C", "D", "E", "F", "G",
-            "H", "I", "J", "K", "L", "M", "N",
-            "O", "P", "Q", "R", "S", "T",
-            "U", "V", "W", "X", "Y", "Z"
+            "S", "H", "Y", "C"
         };
         input = input.toUpper();
         if (kLabels.contains(input))
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -76,10 +76,7 @@ MainWindow::MainWindow(QWidget *parent) :
     {
         const static QStringList kLabels {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-            "A", "B", "C", "D", "E", "F", "G",
-            "H", "I", "J", "K", "L", "M", "N",
-            "O", "P", "Q", "R", "S", "T",
-            "U", "V", "W", "X", "Y", "Z"
+            "S", "H", "Y", "C"
         };
 
         bool ok;
